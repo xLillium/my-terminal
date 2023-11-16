1071941e01dbdeaeca74d06e8da118d46207bdcd
xLillium <nicolas.motillon@gmail.com>
2023-11-16 17:10:44 +0100

    feat: Add typing feature

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Terminal.css';
 
 const Terminal = ({ setShowTerminal }) => {
     const [isFullScreen, setIsFullScreen] = useState(false);
+    const [input, setInput] = useState('');
+    const [history, setHistory] = useState([]);
+    const inputRef = useRef(null);
 
     const handleExitClick = () => {
         const userConfirmed = window.confirm("Are you sure you want to quit?");
@@ -28,17 +31,46 @@ const Terminal = ({ setShowTerminal }) => {
         setIsFullScreen(!isFullScreen);
     };
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            setHistory([...history, input]);
+            setInput('');
+        }
+    };
+
+    const focusInput = () => {
+        inputRef.current.focus();
+    };
+
+    useEffect(() => {
+        focusInput();
+    }, []);
+
     const terminalClassName = isFullScreen ? "terminal terminal-fullscreen" : "terminal";
 
     return (
-        <div className={terminalClassName}>
+        <div className={terminalClassName} onClick={focusInput}>
             <div className="terminal-header">
                 <button className="terminal-button red" onClick={handleExitClick} title="Close Terminal" aria-label="Close Terminal"></button>
                 <button className="terminal-button yellow" onClick={handleMinimizeClick} title="Minimize Terminal" aria-label="Minimize Terminal"></button>
                 <button className="terminal-button green" onClick={handleFullScreenClick} title="Toggle Fullscreen" aria-label="Toggle Fullscreen"></button>
             </div>
             <div className="terminal-body">
-                $ <span className="blinking-cursor">|</span>
+                {history.map((line, index) => (
+                    <div key={index}>$ {line}</div>
+                ))}
+                <div>
+                    $ <input
+                        type="text"
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        onKeyPress={handleKeyPress}
+                        className="terminal-input"
+                        ref={inputRef}
+                        style={{ background: 'transparent' }}
+                        autoFocus
+                    />
+                </div>
             </div>
         </div>
     );
